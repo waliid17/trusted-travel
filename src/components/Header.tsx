8cc3e1742e3d5610b8e5fff872da8b112facb2ca
waliid17 <walidkheloufi00@gmail.com>
2025-02-01 21:48:35 +0100

    new

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';  
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import logo from '../assets/logo.png';
 
 
